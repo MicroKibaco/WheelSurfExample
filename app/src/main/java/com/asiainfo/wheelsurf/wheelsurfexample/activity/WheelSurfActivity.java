125d7f6d741847071bb46894973f1c2aad4b8d80
MicroKibaco <569015640@qq.com>
2017-01-18 16:51:43 +0800

    Initial commit:设置圆形背景色

---
+++ app/src/main/java/com/asiainfo/wheelsurf/wheelsurfexample/activity/WheelSurfActivity.java
@@ -0,0 +1,15 @@
+package com.asiainfo.wheelsurf.wheelsurfexample.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.asiainfo.wheelsurf.wheelsurfexample.R;
+
+public class WheelSurfActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wheel_surf);
+    }
+}
